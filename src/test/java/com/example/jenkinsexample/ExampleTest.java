06c9922fdefad667848be9daea8f2aa57f81efdb
KIMSIYOUNG <tldud2404@gmail.com>
2020-07-21 21:44:23 +0900

    깨지는 테스트 삭제

--- src/test/java/com/example/jenkinsexample/ExampleTest.java
+++ src/test/java/com/example/jenkinsexample/ExampleTest.java
@@ -5,8 +5,5 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class ExampleTest {
-    @Test
-    void failTest() {
-        assertThat(1==0).isTrue();
-    }
+
 }
